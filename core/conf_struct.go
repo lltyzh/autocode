befbcd80646d9ed99a0b6404af63efa654268d62
郭亮亮 <936582100@qq.com>
2021-04-29 14:09:51 +0800

    调整结构

--- core/conf_struct.go
+++ core/conf_struct.go
@@ -7,35 +7,35 @@ type Template struct {
 }
 type Insert struct {
 	BaseTem
-	Position string `json:"position"`
-	Tag      string `json:"tag"`
+	Position string `json:"position"` //插入位置 top bottom left right
+	Tag      string `json:"tag"`      //插入位置的标识
 }
 type Project struct {
-	TplEnd    string `json:"tpl_end"`
-	TplBegin  string `json:"tpl_begin"`
-	InsertTag string `json:"insert_tag"`
-	Name      string `json:"name"`
+	TplEnd    string `json:"tpl_end"`    //会覆盖全局配置
+	TplBegin  string `json:"tpl_begin"`  //会覆盖全局配置
+	InsertTag string `json:"insert_tag"` //会覆盖全局配置
+	Name      string `json:"name"`       //项目名称，不为default时，输入命令格式：./autocode.exe 项目名称 -mdoel ···
 	Params    []struct {
-		Name    string `json:"name"`
-		Des     string `json:"des"`
-		Default string `json:"default"`
-		Verify  string `json:"verify"`
+		Name    string `json:"name"`    //参数名 -参数名  参数值
+		Des     string `json:"des"`     //参数描述
+		Default string `json:"default"` //默认值
+		Verify  string `json:"verify"`  //验证，目前仅支持required不为空
 	}
-	Templates []Template `json:"templates"`
-	Inserts   []Insert   `json:"inserts"`
-	Plugs     []Plug     `json:"plugs"`
+	Templates []Template `json:"templates"` //执行的模板
+	Inserts   []Insert   `json:"inserts"`   //执行的插入
+	Plugs     []Plug     `json:"plugs"`     //执行的插件
 }
 type Plug struct {
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	Commond string `json:"commond"`
-	Params  string `json:"params"`
+	Name    string `json:"name"`    //插件名
+	Type    string `json:"type"`    //插件类型，仅支持shell
+	Commond string `json:"commond"` //命令
+	Params  string `json:"params"`  //参数
 }
 type Config struct {
-	TplEnd    string    `json:"tpl_end"`
-	TplBegin  string    `json:"tpl_begin"`
-	Projects  []Project `json:"projects"`
-	InsertTag string    `json:"insert_tag"`
+	TplEnd    string    `json:"tpl_end"`    //语法结束标签
+	TplBegin  string    `json:"tpl_begin"`  //语法开始标签
+	Projects  []Project `json:"projects"`   //执行的项目列表
+	InsertTag string    `json:"insert_tag"` //插入标识
 }
 type TemInterface interface {
 	SetFile(string)
@@ -48,12 +48,12 @@ type TemInterface interface {
 }
 
 type BaseTem struct {
-	TplEnd    string `json:"tpl_end"`
-	TplBegin  string `json:"tpl_begin"`
-	Template  string `json:"template"`
-	Target    string `json:"target"`
-	Condition string `json:"condition"`
-	Filter    string `json:"filter"`
+	TplEnd    string `json:"tpl_end"`   //语法结束标签
+	TplBegin  string `json:"tpl_begin"` //语法开始标签
+	Template  string `json:"template"`  //模板文件
+	Target    string `json:"target"`    //目标文件或目录
+	Condition string `json:"condition"` //执行条件，内容为模板语法，不用带标签
+	Filter    string `json:"filter"`    //过滤器，暂不支持
 	IsDir     bool
 }
 
